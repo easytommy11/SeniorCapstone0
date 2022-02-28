0ae583be4248b4187c2d5c71d814c0c2b7ef0d8a
Tomiwa Babalola <tbabalola004@gmail.com>
2022-02-28 11:04:22 -0500

    Initial Commit

---
+++ SeniorCapstone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeniorCapstone2
+//
+//  Created by Tomiwa Babalola on 2/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
