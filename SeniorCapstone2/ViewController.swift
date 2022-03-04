19ff421c99316ba39ec1dfeb7d0394f6671d3d93
Tomiwa Babalola <tbabalola004@gmail.com>
2022-03-04 09:46:22 -0500

    settings

--- SeniorCapstone2/ViewController.swift
+++ SeniorCapstone2/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
     func setUpElements() {
         Utilities.styleFilledButton(signUpButton)
-        Utilities.styleFilledButton(loginButton)
+        Utilities.styleHollowButton(loginButton)
     }
 
 
